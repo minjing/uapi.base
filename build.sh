f4a5af7e34e93e351c35fc2dc9ded48c21a3ff43
minjing <min_jing@aliyun.com>
2018-10-24 17:25:08 +0800

    Add option to build script to reduce distbution size

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@ rm -rf dist
 jlink --module-path uapi.common/build/libs:uapi.exception/build/libs:uapi.test/build/libs \
       --add-modules uapi.common,uapi.exception,uapi.test \
       --launcher uapiDemo=uapi.test/uapi.test.Main \
-      --output dist
+      --output dist \
+      --strip-debug \
+      --compress 2 \
+      --no-header-files \
+      --no-man-pages
